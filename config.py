f983383a2412b6b466c39143e86fa30f7eb1286b
mauromanuek <mauromkmanuel@gmail.com>
2026-01-11 18:23:21 +0100

    Update config.py

--- config.py
+++ config.py
@@ -1,14 +1,5 @@
 import os
 
-class Config:
-    # O APP_ID padrão da Deriv para apps genéricos é 1089, 
-    # ou o seu específico se você criou um no portal de desenvolvedores.
-    APP_ID = "114910" 
-    
-    # Este campo ficará vazio aqui por segurança. 
-    # O Bot vai receber o Token que você digitar na tela de Login.
-    DERIV_TOKEN = os.environ.get('DERIV_TOKEN', '') 
-
-    # Configurações de Gerenciamento de Risco Padrão
-    DEFAULT_SYMBOL = "R_100"  # Volatility 100 Index
-    MIN_PROBABILITY = 0.75    # Só avisa se a chance for maior que 75%
+APP_ID = 114910
+DEFAULT_SYMBOL = "R_100"  # Volatility 100 Index
+PORT = int(os.environ.get("PORT", 8000))
