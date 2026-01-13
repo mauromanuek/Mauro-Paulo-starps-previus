063ae502d1db6e7277257d570ab3c0b9e49c56ba
mauromanuek <mauromkmanuel@gmail.com>
2026-01-13 18:56:45 +0100

    Update deriv_api.js

--- src/core/deriv_api.js
+++ src/core/deriv_api.js
@@ -1,42 +1,29 @@
-// src/core/deriv_api.js
 const DerivAPI = {
-    ws: null,
+    socket: null,
     app_id: 114910,
-    state: { balance: 0, currency: 'USD', is_virtual: true, ticks: [] },
 
-    connect(token, onMessage) {
-        this.ws = new WebSocket(`wss://ws.derivws.com/websockets/v3?app_id=${this.app_id}`);
-        
-        this.ws.onopen = () => {
-            this.ws.send(JSON.stringify({ authorize: token }));
-        };
-
-        this.ws.onmessage = (msg) => {
+    connect(token, callback) {
+        this.socket = new WebSocket(`wss://ws.derivws.com/websockets/v3?app_id=${this.app_id}`);
+        this.socket.onopen = () => this.socket.send(JSON.stringify({ authorize: token }));
+        this.socket.onmessage = (msg) => {
             const data = JSON.parse(msg.data);
-            if (data.msg_type === 'authorize' && !data.error) {
-                // Subscrever a saldo e ticks após autorizar
-                this.ws.send(JSON.stringify({ balance: 1, subscribe: 1 }));
-                this.ws.send(JSON.stringify({ ticks: 'R_100', subscribe: 1 }));
+            if(data.msg_type === 'authorize') {
+                this.socket.send(JSON.stringify({ balance: 1, subscribe: 1 }));
+                this.socket.send(JSON.stringify({ ticks: 'R_100', subscribe: 1 }));
             }
-            onMessage(data);
+            callback(data);
         };
     },
 
-    sendOrder(type, amount, barrier) {
+    // Item 6: Envio de ordens reais
+    buyContract(type, amount, symbol) {
         const proposal = {
-            buy: 1,
-            price: amount,
-            parameters: {
-                amount: amount,
-                basis: 'stake',
-                contract_type: type, // Ex: 'DIGITOVER', 'CALL', etc
-                currency: 'USD',
-                duration: 1,
-                duration_unit: 't',
-                symbol: 'R_100'
-            }
+            proposal: 1, amount: amount, barrier: "0", 
+            basis: "stake", contract_type: type, 
+            currency: "USD", duration: 1, duration_unit: "t", 
+            symbol: symbol
         };
-        if(barrier !== undefined) proposal.parameters.barrier = barrier;
-        this.ws.send(JSON.stringify(proposal));
+        this.socket.send(JSON.stringify(proposal));
+        app.notify(`Enviando ordem ${type} de $${amount}...`);
     }
 };
